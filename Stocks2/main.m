6418845521ed3269fddbb700b45154b841fbf464
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-12 16:09:13 -0700

    Initial Commit

---
+++ Stocks2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stocks2
+//
+//  Created by Jamie on 2018-07-12.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
